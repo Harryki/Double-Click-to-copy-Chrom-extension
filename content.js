212aedb18145a45c5c80aa1a1c1378758bba2de1
Harryki <zgp7777@gmail.com>
2017-07-31 01:16:57 -0700

    got rid of prompt
    it copies to your clipboard right away

--- content.js
+++ content.js
@@ -3,38 +3,31 @@ var listener = function() {
     if (selection.rangeCount > 0) {
         var range = selection.getRangeAt(0);
         var text = range.cloneContents().textContent;
-        // console.log(text)
+        //console.log(text);
         // alert()
         // var BodyHTML = document.body.innerHTML;
 
-        var textFromPrompt = window.prompt("Copy to clipboard", text);
-        if(textFromPrompt != null){
-            // alert("copied to clipboard")
-            try {
+        if(text != null){
+            try{
                 var successful = document.execCommand('copy');
-                var msg = successful ? 'successful' : 'unsuccessful';
-                console.log('Copying text command was ' + msg);
-            } catch (err) {
+            }catch (err){
                 console.log('Oops, unable to copy');
             }
         }
 
 
-        // function highlightText(element) {
-        //     var nodes = element.childNodes;
-        //     for (var i = 0, l = nodes.length; i < l; i++) {
-        //         if (nodes[i].nodeType === 3) {
-        //             // Node Type 3 is a text node
-        //             var text = nodes[i].innerHTML;
-        //             nodes[i].innerHTML = "<span style='background-color:'#FFEA0'>" + text + "</span>";
-        //         }
-        //         else if (nodes[i].childNodes.length > 0) {
-        //             highlightText(nodes[i]);  // Not a text node or leaf, so check it's children
-        //         }
+        // var textFromPrompt = window.prompt("Copy to clipboard", text);
+
+        // if(textFromPrompt != null){
+        //     // alert("copied to clipboard")
+        //     try {
+        //         var successful = document.execCommand('copy');
+        //         // var msg = successful ? 'successful' : 'unsuccessful';
+        //         // console.log('Copying text command was ' + msg);
+        //     } catch (err) {
+        //         console.log('Oops, unable to copy');
         //     }
         // }
-
-
 
     };
 
